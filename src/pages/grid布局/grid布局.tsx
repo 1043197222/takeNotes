fc95381ecc36009ba4274cb4bb31744784a6ea28
zhangc_hz@starsino.com <1043197222@qq.com>
2023-05-25 16:37:54 +0800

    添加grid布局笔记

--- src/pages/grid布局/grid布局.tsx
+++ src/pages/grid布局/grid布局.tsx
@@ -2,99 +2,87 @@ import './grid布局.scss';
 
 const Grid = () => {
     return <div>
-        <h3>指定一个三行三列的网格，列宽和行高都是100px</h3>
         <div>
-            <div className={"case-1"}>
-                <div className={"item"}><p>1</p></div>
-                <div className={"item"}><p>2</p></div>
-                <div className={"item"}><p>3</p></div>
-                <div className={"item"}><p>4</p></div>
-                <div className={"item"}><p>5</p></div>
-                <div className={"item"}><p>6</p></div>
-                <div className={"item"}><p>7</p></div>
-                <div className={"item"}><p>8</p></div>
-                <div className={"item"}><p>9</p></div>
-            </div>
-        </div>
-        <div className={"divider"}></div>
-        <h3>指定一个三行三列的网格，使用repeat宽高平均分配N个</h3>
-        <div>
-            <div className={"case-2"}>
-                <div className={"item"}><p>1</p></div>
-                <div className={"item"}><p>2</p></div>
-                <div className={"item"}><p>3</p></div>
-                <div className={"item"}><p>4</p></div>
-                <div className={"item"}><p>5</p></div>
-                <div className={"item"}><p>6</p></div>
-                <div className={"item"}><p>7</p></div>
-                <div className={"item"}><p>8</p></div>
-                <div className={"item"}><p>9</p></div>
-            </div>
-        </div>
-        <div className={"divider"}></div>
-        <h3>指定一个三行三列的网格，使用repeat 分配固定宽度</h3>
-        <h5>repeat(2, 100px 20px 80px) 循环两次 每次三个宽度分别是100 20 80</h5>
-        <div>
-            <div className={"case-3"}>
-                <div className={"item"}><p>1</p></div>
-                <div className={"item"}><p>2</p></div>
-                <div className={"item"}><p>3</p></div>
-                <div className={"item"}><p>4</p></div>
-                <div className={"item"}><p>5</p></div>
-                <div className={"item"}><p>6</p></div>
-                <div className={"item"}><p>7</p></div>
-                <div className={"item"}><p>8</p></div>
-                <div className={"item"}><p>9</p></div>
-            </div>
-        </div>
-        <div className={"divider"}></div>
-        <h3>子元素固定大小，父元素自动填充 直到父元素放不下</h3>
-        <h5>repeat(auto-fill, 130px)</h5>
-        <div>
-            <div className={"case-4"}>
-                <div className={"item"}><p>1</p></div>
-                <div className={"item"}><p>2</p></div>
-                <div className={"item"}><p>3</p></div>
-                <div className={"item"}><p>4</p></div>
+            <h1>容器属性</h1>
+            <h5> display: grid;</h5>
+            把盒子变成网格
+            <h2>grid-template-rows grid-template-columns</h2>
+            每行成N列 两个属性使用方式一样 一个设置行 一个设置列
+            <h4>以下示例包括 repeat minmax fr auto-fill</h4>
 
+            <h3>repeat()</h3>
+            <div>接收两个参数
+                第一个参数重复的次数
+                第二个参数是重复的大小
             </div>
-        </div>
+            <h3>auto-fill</h3>
+            <div>
+                不确定子元素能在父元素放多个个时使用repeat(auto-fill, 130px)自动填充
+            </div>
+            <h3>fr 关键字</h3>
+            <div>
+                与fle布局 fle:1 一样
+            </div>
+            <h3>minmax()</h3>
+            <div>
+                接收两个参数 最小值和最大值
+            </div>
+            <h3>auto 关键字</h3>
+            <div>
+                自动填充
+            </div>
+            <div>
+                <div>示例1 grid-template-rows: 100px 100px 100px</div>
+                <div>示例2 grid-template-rows: repeat(3, 1fr)</div>
+                <div>示例3 grid-template-columns: repeat(2, 100px 20px 80px)</div>
+                <div>示例4 grid-template-columns: repeat(auto-fill, 130px)</div>
+                <div>示例5 grid-template-columns: 1fr 2fr</div>
+                <div>示例6 grid-template-columns:120px 1fr 2fr</div>
+                <div>示例7 grid-template-columns: 1fr 1fr minmax(100px, 1fr)</div>
+            </div>
 
-        <div className={"divider"}></div>
-        <h3>fr 分配到份数</h3>
-        <h5>示例 grid-template-columns: 1fr 2fr</h5>
-        <h5>代表每行两列，总宽度分成3份，第一个占1份，第二个占2份</h5>
-        <div>
-            <div className={"case-5"}>
-                <div className={"item"}><p>1</p></div>
-                <div className={"item"}><p>2</p></div>
-                <div className={"item"}><p>3</p></div>
-                <div className={"item"}><p>4</p></div>
-                <div className={"item"}><p>5</p></div>
-                <div className={"item"}><p>6</p></div>
-                <div className={"item"}><p>7</p></div>
-                <div className={"item"}><p>8</p></div>
-                <div className={"item"}><p>9</p></div>
-            </div>
+            <h2>grid-row-gap grid-column-gap grid-gap</h2>
+            <h5>grid-gap 是前两种的简写</h5>
+            <h5>
+                grid-row-gap 设置行间距
+                grid-column-gap 设置列间距
+            </h5>
+            <h2>grid-auto-flow</h2>
+            <h5>grid-gap 是前两种的简写</h5>
+            <h5>
+                两个参数值 第一个参数 row|column 第二个参数dense
+                默认行排列 row
+                dense 的作用是尽可能的填满容器 中间不出现空格
+            </h5>
+            <h2>justify-items align-items place-items</h2>
+            <h5>设置单元格内容的水平位置 </h5>
+            <h5>place-items 是前两个简写 </h5>
+            <h5>
+                值 start | end | center | stretch 默认stretch
+                用法与flex 一致
+            </h5>
+            <h2>justify-content align-content place-content</h2>
+            <h5>整个内容区域在容器里面的水平位置 </h5>
+            <h5>place-content 是前两个简写 </h5>
+            <h5>
+                值 start | end | center | stretch | space-around | space-between | space-evenly
+                用法与flex 一致
+            </h5>
         </div>
+        <h1>子元素属性</h1>
+        <h2>grid-row-start grid-row-end grid-column-start grid-column-end</h2>
+        <h3>简写 grid-column: 开始位置 / 结束位置</h3>
+        <h3>简写 grid-row: 开始位置 / 结束位置</h3>
+        <h5>start从第几个边框线开始 end到第几个边框线结束 </h5>
+        <h5>span代表跨越元素到个数 </h5>
+        <h5>示例 grid-column-start: span 2 </h5>
 
-        <div className={"divider"}></div>
-        <h3>fr可以与绝对长度的单位结合使用</h3>
-        <h5>示例 grid-template-columns: 150px 1fr 2fr</h5>
-        <h5>代表每行三列，总宽度分成3份，第一个占150px份，第二个占剩余空间1,第三个占剩余空间2份</h5>
-        <div>
-            <div className={"case-6"}>
-                <div className={"item"}><p>1</p></div>
-                <div className={"item"}><p>2</p></div>
-                <div className={"item"}><p>3</p></div>
-                <div className={"item"}><p>4</p></div>
-                <div className={"item"}><p>5</p></div>
-                <div className={"item"}><p>6</p></div>
-                <div className={"item"}><p>7</p></div>
-                <div className={"item"}><p>8</p></div>
-                <div className={"item"}><p>9</p></div>
-            </div>
-        </div>
+        <h2>justify-self align-self place-self </h2>
+
+        <h5>单元格内容的水平位置</h5>
+        <h5>值 start | end | center | stretch</h5>
+        <h5>place-self 前两个缩写 </h5>
+        <h5>示例 grid-column-start: span 2 </h5>
     </div>
 }
 
--- 知识点/grid布局.md
+++ 知识点/grid布局.md
@@ -1,17 +1,74 @@
-### display: grid 
+### display: grid
+
 设置元素为grid 独占一行
-如果子元素没有给宽高 默认每个高度占50px 
+如果子元素没有给宽高 默认每个高度占50px
+
 ### grid-template-rows
+
 ### grid-template-columns
+
 ### repeat()
+
 接收两个参数
 第一个参数重复的次数
 第二个参数是重复的大小
+
 ### auto-fill 关键字
+
 不确定子元素能在父元素放多个个时使用repeat(auto-fill, 130px)自动填充
+
 ### fr 关键字
+
 示例 grid-template-columns: 1fr 2fr
 代表每行两列，总宽度分成3份，第一个占1份，第二个占2份
 fr可以与绝对长度的单位结合使用
 示例 grid-template-columns: 150px 1fr 2fr
 代表每行三列，总宽度分成3份，第一个占150px份，第二个占剩余空间1,第三个占剩余空间2份
+
+### minmax()
+
+接收两个参数 最小值和最大值
+grid-template-columns: 1fr 1fr minmax(100px, 1fr);
+代表 第三列最小值不能低于100
+
+### auto 关键字
+
+示例 grid-template-columns: 100px auto 100px;
+代表 第二列自动填充
+
+### grid-row-gap grid-column-gap  grid-gap
+
+grid-row-gap 设置行间距
+grid-column-gap 设置列间距
+grid-gap 是grid-column-gap和grid-row-gap的合并简写形式
+grid-gap 第一个值是列的间距 第二个值是行的间距
+如果只写一个值 代表行和列是一样的间距
+grid-row-gap grid-column-gap grid-gap 可以简写成 row-gap column-gap gap
+
+### grid-auto-flow
+
+两个参数值 第一个参数 row|column 第二个参数dense     
+默认行排列 row
+dense 的作用是尽可能的填满容器 中间不出现空格
+
+### justify-items  align-items place-items
+
+设置单元格内容的水平位置
+
+### justify-content  align-content place-content
+
+整个内容区域在容器
+
+### grid-row-start grid-row-end grid-column-start grid-column-end
+
+简写 grid-column: 开始位置 / 结束位置
+简写 grid-row: 开始位置 / 结束位置
+start从第几个边框线开始 end到第几个边框线结束
+span代表跨越元素到个数
+示例 grid-column-start: span 2
+
+### justify-self  align-self place-self
+
+单元格内容的水平位置
+值 start | end | center | stretch
+place-self 前两个缩写
